53bacb5ac9a7e7e3103783ba94c366bde7bbaa93
Wattanapong Uttayota <beam36770@gmail.com>
2021-07-21 09:20:44 +0700

    initial

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 
 export default function App() {
+  const press = () =>{
+    console.log("pressed")
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={press()}>HI THERE, THIS IS beam!!!!</Text>
+      <Text>HI</Text>
+      <Image source={{
+        width:200,
+        height:200,
+        uri: "https://media.istockphoto.com/photos/hand-holding-mobile-phone-on-white-picture-id936722632"
+      }}/>
       <StatusBar style="auto" />
     </View>
   );
